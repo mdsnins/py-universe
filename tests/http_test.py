97da6561cfd7d9dbd6d8429681de6ff8c09adf34
mdsnins <payload@kaist.ac.kr>
2021-12-20 19:00:53 +0900

    Fix test names for http_test

--- tests/http_test.py
+++ tests/http_test.py
@@ -6,7 +6,7 @@ def test_http_set_token(token, key):
     try:
         x = Http(token, key)
     except Exception as e:
-        fail("set_token", -1, str(e), "", True)
+        fail("http_set_token", -1, str(e), "", True)
     success("http_set_token")
     return x
 
@@ -23,15 +23,15 @@ def test_http_update_access_token(UniverseHttp, access_token):
     try:
         UniverseHttp.UpdateToken(access_token)
     except Exception as e:
-        fail("update_access_token", -1, str(e), "", True)
+        fail("http_update_access_token", -1, str(e), "", True)
     success("http_update_access_token")
     return
 
 def test_http_retrieve_planet_home(UniverseHttp, planet_id = 34):
     code, data, extra = UniverseHttp.Get("https://api.universe-official.io/planet/home", {"planet_id": planet_id})
     if code != 0:
-        fail("retrieve_palent_home", code, data, extra, True)
-    success("retrieve_planet_home")
+        fail("http_retrieve_palent_home", code, data, extra, True)
+    success("http_retrieve_planet_home")
 
 def do_test():
     instance = test_http_set_token(config.TEST_REFRESH_TOKEN, config.JWE_KEY)
